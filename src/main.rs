5ffdecbe802313ec897891b0b38c5be62e211aa5
Severin Polonius <polonius.severin@gmail.com>
2022-10-28 23:16:38 +0200

    tokenization of equation first try

--- src/main.rs
+++ src/main.rs
@@ -37,26 +37,47 @@ enum Operations {
     Modulo,
 }
 // takes spaceless String as builds calculation tree
-fn calculate(instructions: String) -> f32 {
-
-    let mut a: f32;
-    let mut a_String = String::new();
-    let mut operation: Operations;
-    for c in instructions.chars() {
-
-        match c {
-            '0'..='9' => a_String.push(c),
-            '*' => operation = Operations::Multiply,
-            '/' => operation = Operations::Divide,
-            '+' => operation = Operations::Add,
-            '-' => operation = Operations::Subtract,
-            '%' => operation = Operations::Modulo,
-            _ => panic!("Unknown character '{}'!", c)
+pub fn tokenize(instructions: String) -> Vec<String> {
+    let ops: Vec<char> = vec!['(', ')', '/', '*', '+', '-', '%', '[', ']', '{', '}'];
+    let mut result: Vec<String> = Vec::new();
+    let mut stream = instructions.trim().chars();
+    let mut c: char;
+    let mut token = String::new();
+    loop {
+        if let Some(next_c) = stream.next() {
+            c = next_c;
         }
+        else {
+            return result;
+        }
+
+        if ('0'..='9').contains(&c) | (c == '.') | (c == '-') {
+            token.push(c);
+            loop{ 
+                if let Some(next_c) = stream.next() {
+                    c = next_c;
+                }
+                else {
+                    return result;
+                }
+                if ('0'..='9').contains(&c) | (c == '.') {
+                    token.push(c);
+                }
+                else {
+                break;
+                }
+            }
+            result.push(token);
+            token = String::new();
+            continue;
+        }
+        if ops.contains(&c) {
+            result.push(c.to_string());
+            continue;
+        }
+        if c == ' ' {continue;}
+        panic!("Encountered illegal char '{}' !", c );
     }
-
-
-    return 1.234 as f32;
 }
 
 fn main() {
@@ -64,9 +85,44 @@ fn main() {
     let calc: Vec<String> = args.calc;
 
     let calc_string: String = calc.join("");
-    
-    let result: f32 = calculate(calc_string);
+
+    let tokens = tokenize(calc_string);
+    let result = 1.2345 as f32;
     // print the result with specified precision
     println!("{:.1$}", result, args.precision as usize);
-    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::tokenize;
+    macro_rules! vec_of_strings {
+    ($($x:expr),*) => (vec![$($x.to_string()),*]);
+}
+
+    #[test]
+    fn test_tokenize_empty() {
+        let instructions = String::new();
+        let result: Vec<String> = Vec::new();
+        assert_eq!(result, tokenize(instructions));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_tokenize_neg_illegal_char() {
+        let instructions = "1.23 / 234e10".to_owned();
+        println!("{:#?}", tokenize(instructions));
+    }
+
+    #[test]
+    fn test_tokenize_number() {
+        let instructions = "-167.89".to_owned();
+        let result: Vec<String> = vec_of_strings!("-1", "/", "+", "67", "%", "89");
+        assert_eq!(result, tokenize(instructions));
+    }
+    #[test]
+    fn test_tokenize_string() {
+        let instructions = "-1 / 2.45 + 67 % 89".to_owned();
+        let result: Vec<String> = vec_of_strings!("-1", "/", "2.45",  "+", "67", "%", "89");
+        assert_eq!(result, tokenize(instructions));
+    }
 }
